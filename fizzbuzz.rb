dc232f03b0b63b3cbcdc692a19720d0c68854f4c
Endya Watson <watson.endya@gmail.com>
2018-08-19 20:45:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
- if fizz_3 == fizzbuzz(3)
+def fizzbuzz(int)
+ if int % 3 == 0
  puts "Fizz" 
- elsif fizz_5 == fizzbuzz(5)
+ elsif int % 5 == 0
  puts "Buzz"
- else fizz_15 == fizzbuzz(15)
+ else int % 3 == 0 && 
    puts "Fizzbuzz"
  end
 end
