30ac03f8b995ed7050f111595fdfd9e237bad036
Endya Watson <watson.endya@gmail.com>
2018-08-19 20:39:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
- if fizzbuzz(3)
+ if f
  puts "Fizz" 
  elsif fizzbuzz(5)
  puts "Buzz"
